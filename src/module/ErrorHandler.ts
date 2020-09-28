18b7ab10213fb62ee16b43596d3820ca686e3fe2
markLin <mark-zhili.lin@cn.abb.com>
2020-09-28 15:28:25 +0800

    commit 3

--- src/module/ErrorHandler.ts
+++
@@ -1,9 +0,0 @@
-import {message} from "antd";
-import {ErrorListener, Exception} from "core-fe";
-import {SagaIterator} from "redux-saga";
-
-export class ErrorHandler implements ErrorListener {
-    *onError(exception: Exception): SagaIterator {
-        message.error(exception.message, 5);
-    }
-}
--- src/type/state.ts
+++
@@ -1,8 +0,0 @@
-import {State} from "core-fe";
-import {HomeState} from "module/home";
-
-export interface RootState extends State {
-    app: {
-        home: HomeState;
-    };
-}
